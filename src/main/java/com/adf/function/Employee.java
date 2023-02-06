6f6866f1ba8deea07f39479fabc498610e3f4f37
root <root@DevVM.j2trfkihsp2e5biqacohvqyw4a.bx.internal.cloudapp.net>
2023-02-06 15:48:45 +0000

    first commit

---
+++ src/main/java/com/adf/function/Employee.java
@@ -0,0 +1,47 @@
+package com.batch.example.model;
+
+import java.util.Date;
+
+public class Employee {
+
+	private Integer id;
+	private String firstname;
+	private String lastname;
+	private String address;
+	private String city;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
+	
+}
